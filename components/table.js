5dbaacfdc23c15e83bdcf0f0a5c73ae8b9e72462
Adarsh <adarshkumar167@gmail.com>
2023-11-04 01:33:02 +0530

    Table_done

---
+++ components/table.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, ScrollView, TextInput} from 'react-native';
+import {Table, TableWrapper, Row} from 'react-native-table-component';
+
+const numColumns = 6;
+const numRows = 10;
+
+export default class ExampleThree extends Component {
+  constructor(props) {
+    super(props);
+
+    const tableHead = Array.from({length: numColumns}, (_, i) =>
+      i === 0 ? '' : String.fromCharCode(64 + i),
+    );
+    this.state = {
+      tableHead: tableHead,
+      widthArr: Array(numColumns).fill(100),
+    };
+  }
+
+  render() {
+    const state = this.state;
+    const tableData = [];
+
+    for (let i = 1; i <= numRows; i++) {
+      const rowData = [i.toString()];
+      for (let j = 1; j < state.tableHead.length; j++) {
+        rowData.push(
+          <TextInput key={j} style={styles.input} editable={true} />,
+        );
+      }
+      tableData.push(rowData);
+    }
+
+    return (
+      <View style={styles.container}>
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <Row
+                data={state.tableHead}
+                widthArr={state.widthArr}
+                style={styles.header}
+                textStyle={styles.text}
+              />
+            </Table>
+            <ScrollView style={styles.dataWrapper}>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                {tableData.map((rowData, index) => (
+                  <Row
+                    key={index}
+                    data={rowData}
+                    widthArr={state.widthArr}
+                    style={[styles.row]}
+                    textStyle={styles.text}
+                  />
+                ))}
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {flex: 1, padding: 16, paddingTop: 30},
+  header: {height: 30, backgroundColor: 'lightgrey'},
+  text: {textAlign: 'center', fontWeight: '200', color: 'black', fontSize: 15},
+  row: {height: 30},
+  input: {
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: '200',
+    fontSize: 15,
+    color: 'black',
+    backgroundColor: 'white',
+    padding: 0,
+    margin: 5,
+  },
+});
